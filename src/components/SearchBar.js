407e70034400f0485ed59f06b0d1b92dbc95eb5d
Saurabh Gade <saurabhg@Saurabhs-MacBook-Air.local>
2022-06-04 13:21:45 -0400

    refactors Search component with function based comoponent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-class SearchBar extends React.Component{
 
-    state = {searchValue : ''};
+const SearchBar = ({onSubmitForm}) => {
+    const [searchValue, setSearchValue] = useState('')
 
-    onInputChange = (e) => {
-        this.setState({searchValue: e.target.value});
-    }
-
+  
     onFormSubmit = (e) => {
         e.preventDefault();
-
-        this.props.onSubmitForm(this.state.searchValue);
+        onSubmitForm(searchValue);
     }
 
-    render(){
-        return(
-            <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input type="text" value={this.state.searchValue} onChange={this.onInputChange}/>
-                    </div>
-                </form>
-            </div>
-        );
-    }
+   
+    return(
+        <div className="ui segment">
+            <form onSubmit={onFormSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input type="text" value={searchValue} onChange={(e)=> setSearchValue(e.target.value)}/>
+                </div>
+            </form>
+        </div>
+    );
+
+    
 }
+
 
 export default SearchBar;
